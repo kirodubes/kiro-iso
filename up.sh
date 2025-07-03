457ac764ea529703e573e8ee372538b78e44ebf8
Erik Dubois <erik.dubois@gmail.com>
2025-07-03 06:47:51 +0200

    update

--- up.sh
+++ up.sh
@@ -8,7 +8,6 @@ set -eo pipefail
 # Github    : https://github.com/kirodubes
 # Github    : https://github.com/buildra
 # SF        : https://sourceforge.net/projects/kiro/files/
-
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
@@ -27,6 +26,8 @@ set -eo pipefail
 
 # variables and functions
 workdir=$(pwd)
+
+##################################################################################################################
 
 ./change-version.sh
 

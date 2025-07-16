18d9e863d9dd9cb7fbea831a988f57c1f752968a
Erik Dubois <erik.dubois@gmail.com>
2025-07-16 07:30:09 +0200

    update

--- installation-scripts/build-the-iso.sh
+++ installation-scripts/build-the-iso.sh
@@ -79,6 +79,11 @@ echo "Run this script as a user"
 echo
 echo "You can add a personal local repo to the iso build if you want"
 echo "https://www.youtube.com/watch?v=TqFuLknCsUE"
+echo
+echo "You can learn to creat your own iso on the basis of Kiro"
+echo "That project is called Buildra"
+echo "https://youtu.be/3jdKH6bLgUE"
+echo "https://youtu.be/mH52To8DvlI"
 tput sgr0
 echo "################################################################## "
 echo

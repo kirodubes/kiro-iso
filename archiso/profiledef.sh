4eb86b08bc6a47722c0a707ba183e270c1386d5c
Erik Dubois <erik.dubois@gmail.com>
2026-04-15 06:33:36 +0200

    update

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="kiro"
-iso_label="kiro-v26.04.14.01"
+iso_label="kiro-v26.04.15.01"
 iso_publisher="kiro"
 iso_application="Kiro Live/Rescue CD"
-iso_version="v26.04.14.01"
+iso_version="v26.04.15.01"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
@@ -15,7 +15,7 @@ airootfs_image_type="squashfs"
 #airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
 #airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 #airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '6')
-airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '3' '-b' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '6' '-b' '1M')
 bootstrap_tarball_compression=(zstd -19)
 file_permissions=(
   ["/etc/gshadow"]="0:0:400"
